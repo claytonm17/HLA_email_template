8fe61851a81752f1c86d4aa5831072738f928aa1
claytonm17 <133607487+claytonm17@users.noreply.github.com>
2024-05-28 21:38:01 -0400

    Patient class and body function built

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-console.log('HLA');
+class Patient {
+    constructor(firstName, lastName, mrn) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.mrn = mrn;
+    }
+}
+
+function emailBody(donor, organ, sequence, patient, Tcell, Bcell) {
+    // donor: String    || XYZ1234
+    // organ: String    || Heart
+    // sequence: String || 4
+    // patient: Patient || Patient object
+    // Tcell: boolean   || true = pos / false = neg
+    // Bcell: boolean   || true = pos / false = neg
+
+    if (Tcell) {
+        TcellResult = "Positive";
+    } else {
+        TcellResult = "Negative";
+    }
+    if (Bcell) {
+        BcellResult = "Positive";
+    } else {
+        BcellResult = "Negative";
+    }
+
+    const greeting = "Hello all,";
+    const info = `Please see attached flow crossmatch results and match run for deceased ${organ} donor ${donor}:`;
+    const results = `Seq. #${sequence} - ${patient.lastName}, ${patient.firstName} (${patient.mrn}) is T-cell: ${TcellResult} and B-cell: ${BcellResult}`;
+    const outro = "Best,";
+    const email = `
+        ${greeting}\n\n
+        ${info}\n\n
+        ${results}\n\n
+        ${outro}\n\n
+        `;
+    return email;
+}
+
+const pt = new Patient("Luke", "Skywalker", "849356823");
+console.log(emailBody("DarthVader", "Hand", "2", pt, false, false))
